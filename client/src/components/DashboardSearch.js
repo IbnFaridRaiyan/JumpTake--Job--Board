f18a0f858da98cb8f5f16dc924b560345e614b60
RaiyanIbnFarid <r.ibnfarid2894@student.leedsbeckett.ac.uk>
2026-07-03 01:17:45 +0100

    light mode

--- client/src/components/DashboardSearch.js
+++ client/src/components/DashboardSearch.js
@@ -36,13 +36,9 @@ const DashboardSearch = ({ onSearch, placeholder = 'What do you want to search?'
             <div className="input__container">
                 <div className="shadow__input"></div>
                 <button className="input__button__shadow" type="submit" aria-label="Search dashboard">
-                    <svg width="18" height="18" viewBox="0 0 24 24" aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16" aria-hidden="true">
                         <path
-                            d="M21 21l-4.35-4.35m1.35-5.15a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0Z"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
+                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
                         />
                     </svg>
                 </button>
